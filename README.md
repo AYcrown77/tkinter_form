e23c60b9da248e2b77ee80d6c3063a8d605661a5
AYcrown77 <aycrown77@gmail.com>
2024-01-22 21:14:09 +0100

    Add README.md file

---
+++ README.md
@@ -0,0 +1,3 @@
+Create a tkinter GUI form that collects user information such as name,email and age.
+Include validation for email field to ensure that it follows basic email format
+Show user info when Submit Button is clicked
--- tkinter_form.py
+++ tkinter_form.py
@@ -1,15 +1,25 @@
+"""
+Create a tkinter GUI form that collects user information such as name,email and age.
+Include validation for email field to ensure that it follows basic email format
+Show user info when Submit Button is clicked
+"""
+#import Tkinter module
 import tkinter as tk
 from tkinter import messagebox
 
 def form_submit():
+    #Get user information
     name = name_entry.get()
     email = email_entry.get()
     age = age_entry.get()
 
+    #Check for empty fields
     if not name or not email or not age:
         messagebox.showerror("Error","Fields can not be empty")
+    #Validate the email format
     elif '@' not in email and '.' not in email:
         messagebox.showerror("Error","Invalid Email Adreess")
+    #Display user information
     else:
         information = f"Name: {name}\nEmail: {email}\nAge: {age}"
         messagebox.showinfo("User Information",information)
@@ -19,6 +29,7 @@ window = tk.Tk()
 window.title("User Entry Form")
 window.geometry('400x240')
 
+#Show the label and entry field
 name_label= tk.Label(window, text="Name:",font=('times new roman',20,'bold'))
 name_label.grid(row =0, column=0)
 name_entry = tk.Entry(window)
